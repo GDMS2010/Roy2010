1641a59dcdb989b9f6ca3c94141f003b2b94e00d
DarkScooter <scottoweinsmith@gmail.com>
2020-10-25 12:16:36 -0400

    added sounds
    fixed enemy seizing

---
+++ GDMS2010/Assets/Scripts/RagdollController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RagdollController : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody EnemyRB;
+    public NavMeshAgent navAgent;
+    public Enemy enemy;
+    public BoxCollider boxCollider;
+
+    private Rigidbody[] rigidbodies;
+    private Collider[] colliders;
+
+    private void Awake()
+    {
+        rigidbodies = GetComponentsInChildren<Rigidbody>();
+        colliders = GetComponentsInChildren<Collider>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            ActivateRagdoll();
+        }
+    }
+    private void SetCollidersEnabled(bool enabled)
+    {
+        foreach (Collider col in colliders)
+        {
+            col.enabled = enabled;
+        }
+    }
+
+    private void SetRigidbodiesKinematic(bool kinematic)
+    {
+        foreach (Rigidbody rb in rigidbodies)
+        {
+            rb.isKinematic = kinematic;
+        }
+    }
+
+    public void ActivateRagdoll()
+    {
+        boxCollider.enabled = false;
+        //EnemyRB.isKinematic = true;
+        navAgent.enabled = false;
+        enemy.enabled = false;
+        anim.enabled = false;
+
+        SetCollidersEnabled(true);
+        SetRigidbodiesKinematic(false);
+    }
+}
